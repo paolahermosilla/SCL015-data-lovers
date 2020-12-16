5a3988d096dbcab9608d07c9c98f7bc9b1675936
cecyramallo <cecyramallo@gmail.com>
2020-12-16 17:49:41 -0400

    Incluye listado de 251 pokémon en la segunda página

--- src/main.js
+++ src/main.js
@@ -1,19 +1,27 @@
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-
-let array = data.pokemon;
-
-for(let i = 0; i < array.length; i++){
-     console.log(array[i])
-}
-
-document.getElementById("pizarra").innerHTML = data.pokemon[0];
-
-//console.log(data.pokemon);
 // import data from './data/rickandmorty/rickandmorty.js';
 
+import data from './data/pokemon/pokemon.js';
 
+function pokemonList(){
+     let array = data.pokemon;
+     let list = "";
+     for(let i = 0; i < array.length; i++){  
+          list += array[i].num+": "+array[i].name+". Type: "+array[i].type+". Size: "
+          +array[i].size.height+", "+array[i].size.weight+"."+"<br>";
+
+          console.log(array[i].name);
+          console.log(array[i].type);
+          console.log(array[i].size);
+     }
+
+     return list;
+}
+
+document.getElementById("pokemon_list").innerHTML = pokemonList();
+
+console.log(data.pokemon);
 
 document.getElementById("enter_button").addEventListener("click", function(){
      document.getElementById("page_one").style.display = "none";
