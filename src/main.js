ad244f229a1f2f00e64511222324cbbee4d9475a
cecyramallo <cecyramallo@gmail.com>
2020-12-17 17:25:23 -0400

    Intentos de manipulación de data

--- src/main.js
+++ src/main.js
@@ -4,12 +4,32 @@
 
 import data from './data/pokemon/pokemon.js';
 
+// const dataPokemon = data.pokemon;
+// const printData = (dataPokemonParameter) => {
+//      listPokemon.innerHTML = "";
+//      for(let i = 0; i < dataPokemonParameter.length; i++){  
+//           let card = htmltoElements (
+//                `<div class = "all-card">
+//                     <img src='${dataPokemonParameter[i].img}'/>
+//                     <p>${dataPokemonParameter[i].name}</p>
+//                     <button class="button-card">Ver Perfil</button>
+//                </div>`);
+//           listPokemon.appendChild(card);
+//           card.addEventListener("click", function(){
+//                printModal(dataPokemonParameter[i])
+//                containerModal.style.display = "block";
+//           }) 
+//      }
+// }
+
 function pokemonList(){
      let array = data.pokemon;
      let list = "";
-     for(let i = 0; i < array.length; i++){  
-          list += array[i].num+": "+array[i].name+". Type: "+array[i].type+". Size: "
-          +array[i].size.height+", "+array[i].size.weight+"."+"<br>";
+     for(let i = 0; i < array.length; i++){
+          `<div class = "all-card">
+               list += `<img; src="${array[i].img}">`+array[i].num+": "+array[i].name+". Type: "+array[i].type+". Size: "
+               +array[i].size.height+", "+array[i].size.weight+"."+"<br>";
+          </div>`;
 
           console.log(array[i].name);
           console.log(array[i].type);
@@ -19,7 +39,7 @@ function pokemonList(){
      return list;
 }
 
-document.getElementById("pokemon_list").innerHTML = pokemonList();
+//document.getElementById("pokemon_list").innerHTML = pokemonList();
 
 console.log(data.pokemon);
 
