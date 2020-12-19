3fa271981f2fec176ff797863b4dd0235d3659f9
paolahermosilla <paitohermosillacaro@gmail.com>
2020-12-19 16:23:57 -0300

    .

--- src/main.js
+++ src/main.js
@@ -7,16 +7,17 @@ import data from './data/pokemon/pokemon.js';
 function pokemonList(){
      let array = data.pokemon;
      let list = "";
-     for(let i = 0; i < array.length; i++){  
-          `<div class = "all-card">
-               list += `<img; src="${array[i].img}">`+array[i].num+": "+array[i].name+". Type: "+array[i].type+". Size: "
-               +array[i].size.height+", "+array[i].size.weight+"."+"<br>";
-          </div>`;
-
-
-          console.log(array[i].name);
-          console.log(array[i].type);
-          console.log(array[i].size);
+     for(let i = 0; i < array.length; i++){
+               list += `<div class= "cards">
+               <img src="${array[i].img}">
+               <p><span>${array[i].num}</span>: ${array[i].name}</p>
+               <p>Type: ${array[i].type}</p>
+               <p><span>Size: ${array[i].size.height}</span>, ${array[i].size.weight}</p>
+               </div>`
+          
+          //console.log(array[i].name);
+          //console.log(array[i].type);
+          //console.log(array[i].size);
      }
 
      return list;
@@ -35,3 +36,22 @@ document.getElementById("return_button").addEventListener("click", function(){
      document.getElementById("page_two").style.display = "none";
      document.getElementById("page_one").style.display = "block";
 });
+
+
+// const dataPokemon = data.pokemon;
+// const printData = (dataPokemonParameter) => {
+//      listPokemon.innerHTML = "";
+//      for(let i = 0; i < dataPokemonParameter.length; i++){  
+//           let card = htmltoElements (
+//                `<div class = "all-card">
+//                     <img src='${dataPokemonParameter[i].img}'/>
+//                     <p>${dataPokemonParameter[i].name}</p>
+//                     <button class="button-card">Ver Perfil</button>
+//                </div>`);
+//           listPokemon.appendChild(card);
+//           card.addEventListener("click", function(){
+//                printModal(dataPokemonParameter[i])
+//                containerModal.style.display = "block";
+//           }) 
+//      }
+// }
