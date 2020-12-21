bca5643f85127c455f6677397fdd6203e15a9025
paolahermosilla <paitohermosillacaro@gmail.com>
2020-12-21 08:21:03 -0300

    avance botones pero sin funcion

--- src/main.js
+++ src/main.js
@@ -3,12 +3,24 @@
 // import data from './data/rickandmorty/rickandmorty.js';
 
 import data from './data/pokemon/pokemon.js';
+function searchType(){
+     let array = data.pokemon;
+     let x = document.getElementById("typeSelect").value;
+   
+     for (let i = 0; i < array.length; i++) {
+       if (x === array[i].type) {
+          document.getElementById(array[i].num).style.display = "block";
+   
+       } 
+       }
+   }
+   
 
 function pokemonList(){
      let array = data.pokemon;
      let list = "";
-     for(let i = 0; i < array.length; i++){
-               list += `<div class= "cards">
+     for(let i = 0; i < array.length; i++){//agregamos el id para poder identificarlo luego
+               list += `<div class= "cards" id="${array[i].num}"> 
                <img src="${array[i].img}">
                <p><span>${array[i].num}</span>: ${array[i].name}</p>
                <p>Type: ${array[i].type}</p>
@@ -37,6 +49,27 @@ document.getElementById("return_button").addEventListener("click", function(){
      document.getElementById("page_one").style.display = "block";
 });
 
+document.getElementById("typeSelect").addEventListener("change", searchType());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // const dataPokemon = data.pokemon;
 // const printData = (dataPokemonParameter) => {
@@ -54,4 +87,4 @@ document.getElementById("return_button").addEventListener("click", function(){
 //                containerModal.style.display = "block";
 //           }) 
 //      }
-// }
+//  
