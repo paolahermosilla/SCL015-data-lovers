d38309ce06b497e2836dc73bd2074cfc41ee29cd
paolahermosilla <paitohermosillacaro@gmail.com>
2021-01-05 14:10:09 -0300

    modularizacion:search,order az y za

--- src/data.js
+++ src/data.js
@@ -4,19 +4,35 @@ export const filterData = (array, typePokemon) => {
     const filterType = array.filter(pokemon => {
       return pokemon.type.includes(typePokemon);
     });
+    console.log(filterType)
     return filterType;
 };
 
-export const sortData = () => {
-    const filterOrderA = array.sort((a, b) => {
+export const sortDataAZ = (array) => {
+    const filterOrderAZ = array.sort((a, b) => {
       return a.name > b.name ? 1 : -1 ;
     });
-
-         // let filterOrderZ = array.sort((a, b) => {
-     //      return a.name < b.name ? 1 : -1 ;
-     // });    
+     return filterOrderAZ;
 };
 
+export const sortDataZA = (array) => {
+  const filterOrderZA = array.sort((a, b) => {
+    return a.name < b.name ? 1 : -1 ;
+  });
+   return filterOrderZA;
+};
+
+export const search = (array, text)  => {
+  const filterCharacter = array.filter((character) => {
+    return character.name.includes(text);
+  });
+  return filterCharacter;
+};
+        
+
+ //let filterOrderZ = array.sort((a, b) => {
+     //      return a.name < b.name ? 1 : -1 ;
+     // });
 
 // export const example = () => {
 //   return 'example';
