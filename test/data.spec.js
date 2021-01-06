a1e44c47f144a2b7ad9a63e5d02f48a6825fab1e
paolahermosilla <paitohermosillacaro@gmail.com>
2021-01-06 13:13:54 -0300

    teste funcionando correctamente,realizamos cambios en css para la pag one

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,41 +1,45 @@
 import { filterData, sortDataAZ, sortDataZA, search } from '../src/data.js';
 
 describe('filterData', () => {
+  const pokemon = [{"name": "charmander", "type": ["fire"]}];
   it('is a function', () => {
     expect(typeof filterData).toBe('function');
   });
 
-  // it('returns `example`', () => {
-  //   expect(example()).toBe('example');
-  // });
+   it('returns `charmander when you select type fire`', () => {
+     expect(filterData(pokemon, "fire")).toEqual([{"name": "charmander", "type": ["fire"]}]);
+   });
 });
 
-describe('sortDataAZ', () => {
-  it('is a function', () => {
-    expect(typeof sortDataAZ).toBe('function');
+ describe('sortDataAZ', () => {
+   const pokemon = [{"name": "metapod", "type": ["bug"]}, {"name": "ditto", "type": ["normal"]}, {"name": "voltorb", "type": ["electric"]}];
+   it('is a function', () => {
+     expect(typeof sortDataAZ).toBe('function');
+   });
+
+  it('returns `Pokémon in alphabetical order A to Z`', () => {
+   expect(sortDataAZ(pokemon)).toEqual([{"name": "ditto", "type": ["normal"]}, {"name": "metapod", "type": ["bug"]}, {"name": "voltorb", "type": ["electric"]}]);
   });
-
-  // it('returns `example`', () => {
-  //   expect(example()).toBe('example');
-  // });
-});
+ });
 
 describe('sortDataZA', () => {
+  const pokemon = [{"name": "metapod", "type": ["bug"]}, {"name": "ditto", "type": ["normal"]}, {"name": "voltorb", "type": ["electric"]}];
   it('is a function', () => {
     expect(typeof sortDataZA).toBe('function');
   });
 
-  // it('returns `example`', () => {
-  //   expect(example()).toBe('example');
-  // });
+  it('returns `Pokémon in alphabetical order Z to A`', () => {
+    expect(sortDataZA(pokemon)).toEqual([{"name": "voltorb", "type": ["electric"]}, {"name": "metapod", "type": ["bug"]}, {"name": "ditto", "type": ["normal"]}]);
+  });
 });
 
 describe('search', () => {
+  const pokemon = [{"name": "pikachu"}, {"name": "lapras"}, {"name": "dewgong"}, {"name": "togepi"}]
   it('is a function', () => {
     expect(typeof search).toBe('function');
   });
 
-  // it('returns `example`', () => {
-  //   expect(example()).toBe('example');
-  // });
+  it('returns `pikachu and togepi when searching pi`', () => {
+    expect(search(pokemon, "pi")).toEqual([{"name": "pikachu"}, {"name": "togepi"}]);
+  });
 });
