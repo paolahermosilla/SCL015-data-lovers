1c01f0a86301d59d1941058612960c29e56ea826
cecyramallo <cecyramallo@gmail.com>
2021-01-11 10:33:06 -0400

    Archivos html, CSS y main.js con comentarios

--- src/main.js
+++ src/main.js
@@ -1,20 +1,26 @@
+// To import the array with objects with information and characteristics about all 251 Pokémon
 import data from './data/pokemon/pokemon.js';
+
+// To import the logic of the four functions from data.js that allows the user to interact with the website
 import { filterData } from './data.js';
 import { sortDataAZ } from './data.js';
 import { sortDataZA } from './data.js';
 import { search } from './data.js';
 
+// Event: When clicking on the Pokeball button, you can go from the first page to the second one
 document.getElementById("enter_button").addEventListener("click", function(){
      document.getElementById("page_one").style.display = "none";
      document.getElementById("page_two").style.display = "block";
 });
 
+// Event: When clicking on the house button, you can go from the second page to the first one
 document.getElementById("return_button").addEventListener("click", function(){
      document.getElementById("page_two").style.display = "none";
      document.getElementById("page_one").style.display = "block";
 });
 
 
+// function that displays all 251 Pokémon when you go to the second page
 function pokemonList(array){
      let list = "";
      for(let i = 0; i < array.length; i++){//agregamos el id para poder identificarlo luego
@@ -31,17 +37,20 @@ document.getElementById("pokemon_list").innerHTML = pokemonList(data.pokemon);
 console.log(data.pokemon);
 
 
+// function that orders all 251 Pokémon in alphabetical order (A to Z)
 document.getElementById("orderAZ").addEventListener("click", function alphabeticalOrderAZ(){
     let array = data.pokemon;
     document.getElementById("pokemon_list").innerHTML = pokemonList(sortDataAZ(array));
 });
 
+// function that orders all 251 Pokémon in reverse alphabetical order (Z to A)
 document.getElementById("orderZA").addEventListener("click", function alphabeticalOrderZA(){
     let array = data.pokemon;
     document.getElementById("pokemon_list").innerHTML = pokemonList(sortDataZA(array));
 });
 
 
+// function to select only one specific Pokémon type and that displays that type of Pokémon 
 function searchType(){
      let array = data.pokemon;
      let typePokemon = document.getElementById("type_select").value;
@@ -49,10 +58,12 @@ function searchType(){
 
      document.getElementById("pokemon_list").innerHTML = pokemonList(filterData(array, typePokemon));
 
+     // Event: When clicking on the red button, Pokémon of the selected type are ordered in alphabetical order (A to Z)
      document.getElementById("orderAZ").addEventListener("click", function alphabeticalOrderAZ(){
           document.getElementById("pokemon_list").innerHTML = pokemonList(sortDataAZ(filterData(array, typePokemon)));
      });
-     
+
+     // Event: When clicking on the blue button, Pokémon of the selected type are ordered in reverse alphabetical order (Z to A)
      document.getElementById("orderZA").addEventListener("click", function alphabeticalOrderZA(){
           document.getElementById("pokemon_list").innerHTML = pokemonList(sortDataZA(filterData(array, typePokemon)));
      });
@@ -60,6 +71,7 @@ function searchType(){
 document.getElementById("type_select").addEventListener("change", searchType);
 
 
+// function to search some specific Pokémon depending on the characters the user writes on the search bar
 function filterLetter(){
      let array = data.pokemon;
      let text = document.getElementById("search_box").value.toLowerCase();
@@ -81,7 +93,7 @@ document.getElementById("search_box").addEventListener("keyup", filterLetter);
 
 // const formulario = document.querySelector("#search_box");
 // const boton = document.querySelector("#search_button");
-// const resultado = document.querySelector("pokemon_list")
+// const resultado = document.querySelector("pokemon_list");
 
 // const filtrar = () => {
 //      console.log(formulario.value)
